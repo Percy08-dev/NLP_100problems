e70f0e550cda6a9bcfe0c9ade488d19d442712cd
Percy <great.wooded.house@gmail.com>
2021-10-26 00:10:51 +0900

    fix

--- 準備運動/07.py
+++ 準備運動/07.py
@@ -2,13 +2,14 @@ from typing import Any
 
 
 def form07(x: Any, y: Any, z: Any):
-    print("{}時の{}は{}".format(x, y, z))
+    return "{}時の{}は{}".format(x, y, z)
 
 def main():
     x=12
     y="気温"
     z=22.4
-    form07(x, y, z)
+    ans = form07(x, y, z)
+    print(ans)
 
 if __name__ == "__main__":
     main()
